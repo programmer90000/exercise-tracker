b7f3e6efa277263b7a24477dd07ce71e301e8313
Awwal <sakeebmawwal@gmail.com>
2024-01-18 19:40:41 +0000

    created an object that keeps track of the users  workout data

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,17 @@
+const statistics = 
+[
+    {
+        totalNumberOfWorkOut:2
+    },
+    {
+        totalNumberOfExercise:3
+    },
+    {
+        avgNumberOfExercise:4
+    },
+    {
+        averageNumberOfWorkOutPerWeek:7
+    }
+];
+
+export default statistics;
--- src/pages/statistics.jsx
+++ src/pages/statistics.jsx
@@ -1,1 +1,3 @@
 import React from "react";
+import statistics from "../components/Statistics/Statistics";
+

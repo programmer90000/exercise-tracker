91ea5713eb09296f8feca655876b484d33478981
Abdul Rehman <abdulr568programming@outlook.com>
2024-01-20 18:00:23 +0000

    Define function for downloading JSON data

--- src/components/NewWorkout/NewWorkout.jsx
+++ src/components/NewWorkout/NewWorkout.jsx
@@ -10,6 +10,17 @@ function NewWorkout() {
         reset();
     };
 
+    const downloadJson = () => {
+        const dataStr = JSON.stringify(exercises);
+        const dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr); // Create a data URI for the JSON string
+        const exportFileDefaultName = 'workout.json';
+    
+        const linkElement = document.createElement('a');
+        linkElement.setAttribute('href', dataUri);
+        linkElement.setAttribute('download', exportFileDefaultName);
+        linkElement.click(); // Click the link to download the file
+      };
+
     return (
         <div></div>
     );

e44181165adfef6428dc0fc6c25a1f19f771610c
Fillipe <you@example.com>
2024-01-23 20:42:11 +0000

    navbar now usable, component to navigate between pages completed

--- src/pages/statistics.jsx
+++ src/pages/statistics.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+
+
+const Statistics = () => (
+    <div>
+      <h1>Statistics</h1>
+      <p>
+       Statistics here.
+      </p>
+    </div>
+  );
+  
+  export default Statistics;

9805b034725787c857b6d9b9d2e5d09d544bd1df
ibalan92 <ibalan92@gmail.com>
2024-01-25 16:00:57 +0000

    Added two API calls to get the gym data

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -3,6 +3,46 @@ import './style.css';
 
 const SearchForm = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [locationData, setLocationData] = useState(null);
+  const [gymData, setGymData] = useState(null);
+  const locationAPIkey = '07c6e2c39b20420cb9827bf457518cbc';
+  const numberOfResults = 10;
+
+  useEffect(() => {
+    if (searchTerm !== '') {
+      // First API call to get location data
+      const locationQueryURL = `https://api.geoapify.com/v1/geocode/search?text=${searchTerm}&format=json&apiKey=${locationAPIkey}`;
+      
+      fetch(locationQueryURL)
+        .then(response => response.json())
+        .then(data => {
+          setLocationData(data);
+        })
+        .catch(error => {
+          console.error('Error making location API call:', error.message);
+        });
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (locationData && locationData.results && locationData.results.length > 0) {
+      // Extract place_id from the first result of the location data
+      const placeId = locationData.results[0].place_id;
+
+      // Second API call to get gyms data using place_id
+      const gymQueryURL = `https://api.geoapify.com/v2/places?categories=sport.fitness&filter=place:${placeId}&limit=${numberOfResults}&apiKey=${locationAPIkey}`;
+      
+      fetch(gymQueryURL)
+        .then(response => response.json())
+        .then(data => {
+          setGymData(data);
+          onSearch(data); 
+        })
+        .catch(error => {
+          console.error('Error making gym API call:', error.message);
+        });
+    }
+  }, [locationData, numberOfResults, onSearch]);
 
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);

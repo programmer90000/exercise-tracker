b0812745cadcfb77f5f00706974570a7093a7c0b
ibalan92 <ibalan92@gmail.com>
2024-01-25 14:20:46 +0000

    Built the search page and imported the searchform into it

--- src/pages/nearestGym.jsx
+++ src/pages/nearestGym.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React from 'react';
+import SearchForm from '../components/SearchForm';
 
 
-const NearestGym = () => (
-    <div>
-      <h1>Nearest Gym</h1>
-      <p>
-       Nearest Gym here.
-      </p>
+const NearestGym = () => {
+  const handleSearch = (searchTerm) => {
+    console.log('Searching for:', searchTerm);
+    
+  };
+
+  return (
+    <div className="search-page">
+      <h1>Find nearest gym</h1>
+      <SearchForm onSearch={handleSearch} />
+      
     </div>
   );
+};
   
-  export default NearestGym;
+export default NearestGym;

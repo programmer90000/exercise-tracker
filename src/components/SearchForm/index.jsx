e34a2e572b593b2b60e33fdd7519611804c1fe98
ibalan92 <ibalan92@gmail.com>
2024-01-25 14:00:51 +0000

    Create the initial structure of the SearchForm component

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,2 +1,17 @@
+import React, { useState } from 'react';
+import './style.css';
 
+const SearchForm = ({ onSearch }) => {
+  return (
+    <div className="search-form">
+      <input
+        type="text"
+        placeholder="Enter your city here"
+      />
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default SearchForm;
 

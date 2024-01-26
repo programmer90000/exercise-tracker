06014a4ba82cc7ec92694c2f943a130a3e715285
Abdul Rehman <abdulr568programming@outlook.com>
2024-01-26 15:35:30 +0000

    Fixed images bug

--- src/components/heroImage/heroImage.jsx
+++ src/components/heroImage/heroImage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import fitnessBannerImage from '/fitness-banner-image.jpg';
 
 function HeroImage() {
   return (
     <header style={{ paddingLeft: 0 }}>
       <div className=' text-center bg-image'      >
-        <img src='./public/fitness-banner-image.jpg' alt='Fitness Banner' height={200}></img>
+        <img src={fitnessBannerImage} alt='Fitness Banner' height={200}></img>
       </div>
     </header>
   );
